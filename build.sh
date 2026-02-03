1e741a582866bd797cae6fe886d761065731866d
jeswaa <jeswaaa1803@gmail.com>
2026-02-03 08:55:49 +0800

    Ready for Deployment

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
-set -o errexit
+# exit on error
+set -e
 
+echo "Running Composer..."
 composer install --no-dev --optimize-autoloader
+
+echo "Running NPM..."
 npm install
 npm run build
+
+echo "Running Migrations..."
 php artisan migrate --force
+
+echo "Deployment finished!"
